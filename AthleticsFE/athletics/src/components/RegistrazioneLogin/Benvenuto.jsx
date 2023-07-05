8e64802d3f5b865c259dba93f87871c0a5279719
gretel-hans <adjeihansel@icloud.com>
2023-07-05 21:43:05 +0200

    creazione parte front end

---
+++ AthleticsFE/athletics/src/components/RegistrazioneLogin/Benvenuto.jsx
@@ -0,0 +1,13 @@
+const Benvenuto=()=>{
+    return(
+        <div id="Benvenuto" className="d-flex align-items-center justify-content-center">
+            <div id="BenvenutoDivOne" className="d-flex ">
+            <h1>Benvenuto nella pagina!!</h1>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Benvenuto;
---
+++ AthleticsFE/athletics/src/components/RegistrazioneLogin/Registrazione.jsx
@@ -0,0 +1,7 @@
+const Registrazione=()=>{
+    return(
+        <h1>Registrazione</h1>
+    );
+}
+
+export default Registrazione;

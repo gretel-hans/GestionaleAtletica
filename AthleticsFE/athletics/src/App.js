eb9197cb708010563773c00fcf0aff17e9804342
gretel-hans <adjeihansel@icloud.com>
2023-07-12 20:27:08 +0200

    .

--- AthleticsFE/athletics/src/App.js
+++ AthleticsFE/athletics/src/App.js
@@ -8,7 +8,7 @@ import Login from './components/RegistrazioneLogin/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Homepage from './components/homePage/Homapage';
+import Homepage from './components/HomePage/Homapage';
 
 function App() {
   return (

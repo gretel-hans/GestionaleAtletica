4beb81cb06afa43204ad29f0b6e7d1ab34034985
gretel-hans <adjeihansel@icloud.com>
2023-07-19 09:33:06 +0200

    continua gestione di ricerce su atleti

--- AthleticsFE/athletics/src/components/Ricerche/RicercaAtleti.jsx
+++ AthleticsFE/athletics/src/components/Ricerche/RicercaAtleti.jsx
@@ -1,40 +1,43 @@
 import { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
-import NavbarAthletix from "../HomePage/NavbarAthletix"
+import NavbarAthletix from "../HomePage/NavbarAthletix";
+import Card from "react-bootstrap/Card";
 
-const RicercaAtleti=()=>{
+const RicercaAtleti = () => {
+  let utenteAtleta = [];
+  let [atleti, setAtleti] = useState([{}]);
+  let risultatoAtleti = [];
+  let [inputBarra, setInputBarra] = useState("");
 
-  let atleti=[];
-  let [risultatoAtleti,setRisultatoAtleti]=useState([])
-  let [inputBarra,setInputBarra]=useState("");
-
-  const fetchAtleti=async ()=>{
+  const fetchAtleti = async () => {
     try {
-        let response=await fetch("http://localhost:8080/athletics/utenti");
-        if(response.ok){
-          let data=await response.json();
-          
-          data.forEach((utente,indexUtente) => {
-            let ruoli=[...utente.roles]
-            ruoli.forEach((ruolo,index)=>{
-                 if(ruolo.roleName==="ROLE_ATLETA"){
-                    atleti.push(utente)
-                }
-            })
-          });
-        }
-      } catch (error) {
-        console.log("ERRORE! Durante il caricamento di tutti gli utenti")
-      }
-}
+      let response = await fetch("http://localhost:8080/athletics/utenti");
+      if (response.ok) {
+        let data = await response.json();
 
-useEffect(()=>{
-  fetchAtleti()
-},[])
-  
-    return(
-        <>
-        {(sessionStorage.getItem("username") === null ||
+        data.forEach((utente, indexUtente) => {
+          let ruoli = [...utente.roles];
+          ruoli.forEach((ruolo, index) => {
+            if (ruolo.roleName === "ROLE_ATLETA") {
+              utenteAtleta.push(utente);
+              //console.log("entrato perché è atleta")
+            }
+          });
+        });
+      }
+      setAtleti(utenteAtleta);
+    } catch (error) {
+      console.log("ERRORE! Durante il caricamento di tutti gli utenti");
+    }
+  };
+  //console.log(utenteAtleta)
+  useEffect(() => {
+    fetchAtleti();
+  }, []);
+
+  return (
+    <>
+      {(sessionStorage.getItem("username") === null ||
         sessionStorage.getItem("username") === "null") && (
         <div>
           Non sei loggato per accedere ai contenuti esegui il login o
@@ -44,32 +47,72 @@ useEffect(()=>{
 
       {(sessionStorage.getItem("username") !== null ||
         sessionStorage.getItem("username") !== "null") && (
+        <div>
+          <NavbarAthletix />
 
-          <div>
-            <NavbarAthletix />
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              value={inputBarra}
+              placeholder="Ricerca atleti..."
+              aria-label="Search"
+              onChange={(e) => {
+                console.log(e.target.value);
+                setInputBarra(e.target.value);
+                if (e.target.value === "") {
+                  risultatoAtleti = [...atleti];
+                  console.log(risultatoAtleti);
+                }
+              }}
+            />
+            <Button
+              className="btn btn-outline-success"
+              onClick={() => {
+                risultatoAtleti = [];
+                atleti.forEach((atleta, index) => {
+                  //console.log("nome: "+atleta.nome.toLocaleLowerCase())
+                  if (
+                    (
+                      atleta.nome.toLowerCase() +
+                      " " +
+                      atleta.cognome.toLowerCase()
+                    ).includes(inputBarra.toLocaleLowerCase())
+                  ) {
+                    console.log(atleta);
+                    risultatoAtleti.push(atleta);
+                    // setRisultatoAtleti(...RicercaAtleti,atleta)
+                  }
+                });
+                console.log(risultatoAtleti);
+              }}
+            >
+              Cerca
+            </Button>
+          </form>
+          {  console.log(risultatoAtleti)}
+          ciao
+          {risultatoAtleti.map((atleta, index) => {
+            return (
+              <>
+                <Card style={{ width: "18rem" }} key={index}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Atleta</Card.Title>
+                    <Card.Text>
+                      Nome: {atleta.nome}
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </>
+            );
+          })}
+        </div>
+      )}
+    </>
+    
+  );
+};
 
-        <form className="d-flex" role="search">
-      <input className="form-control me-2" type="search" value={inputBarra} placeholder="Ricerca atleti..." aria-label="Search" onChange={(e)=>{
-        console.log(e.target.value)
-        setInputBarra(e.target.value)
-      }}/>
-      <Button className="btn btn-outline-success" onClick={()=>{
-      atleti.forEach((atleta,index)=>{
-        if((atleta.nome).toLowerCase().includes(inputBarra.toLocaleLowerCase())){
-          console.log(atleta)
-          setRisultatoAtleti(...RicercaAtleti,atleta)
-        }
-      })
-      console.log(atleti)
-console.log("cliccato")
-      }}>Cerca</Button>
-    </form>
-            </div>
-
-
-        )}
-        </>
-    )
-}
-
-export default RicercaAtleti
+export default RicercaAtleti;
